9f59a45519784dfc277a79848b24ec5b4f26e35e
Menyalin Alexey <menyalin@icloud.com>
2020-01-25 15:05:55 +0300

    #

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
+    jest: true
   },
+  plugins: ['jest', 'babel'],
   extends: [
-    'standard'
+    'standard', 'plugin:jest/recommended'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
