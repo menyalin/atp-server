07a760f577539e74e015c2833b6d2de8df3cc0d5
Menyalin Alexey <menyalin@icloud.com>
2020-02-05 22:47:16 +0300

    #

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -7,7 +7,7 @@ const createToken = async (user) => {
   const token = await jwt.sign({
     id: user.id,
     email: user.email
-  }, process.env.JWT_SECRET, { expiresIn: '1d' })
+  }, process.env.JWT_SECRET, { expiresIn: '31d' })
   return token
 }
 
