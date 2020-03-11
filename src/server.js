0f9a8682eaad70e237768adcc5fc53b3c8f39002
Menyalin Alexey <menyalin@icloud.com>
2020-03-11 16:43:30 +0300

    #

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+const cluster = require('cluster')
+const numCpu = require('os').cpus().length
 import 'dotenv/config'
 import './pgDB'
 import cors from 'cors'
@@ -6,7 +8,10 @@ import { ApolloServer } from 'apollo-server-express'
 import schema from './schema'
 import resolvers from './resolvers'
 import context from './context'
+
 const http = require('http')
+
+
 
 const port = process.env.PORT || 3000
 const app = express()
@@ -21,7 +26,20 @@ server.applyMiddleware({ app, path: '/graphql' })
 const httpServer = http.createServer(app)
 server.installSubscriptionHandlers(httpServer)
 
-httpServer.listen({ port }, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Apollo Server on http://localhost:${port}/graphql`)
-})
+if (cluster.isMaster) {
+  for (let i = 0;i < numCpu;i++) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    // eslint-disable-next-line no-console
+    console.log(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  httpServer.listen({ port }, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Apollo Server on http://localhost:${port}/graphql`)
+  })
+}
+
+
+
