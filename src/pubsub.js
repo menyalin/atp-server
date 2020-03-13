aa7e575d6a8c4144db76b88927eef4a4b499123b
Menyalin Alexey <menyalin@icloud.com>
2020-03-13 13:59:17 +0300

    #

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,3 +1,14 @@
 import { PubSub } from 'apollo-server-express'
-
-export const pubsub = new PubSub()
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+export let pubsub
+const options = {
+    host: '192.168.1.47',
+    port: 6379,
+    retryStrategy: times => {
+        // reconnect after
+        return Math.min(times * 50, 2000);
+    }
+};
+if (process.env.ENV_TYPE === 'dev') {
+    pubsub = new PubSub()
+} else pubsub = new RedisPubSub(options);
